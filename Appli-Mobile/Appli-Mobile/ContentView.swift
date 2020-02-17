ab71dc127bdcbe1356d2240e82fbe28b13740052
Justine F <44777911+MatthewHaas19@users.noreply.github.com>
2020-02-17 15:39:09 +0000

    init appli mobile

---
+++ Appli-Mobile/Appli-Mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Appli-Mobile
+//
+//  Created by user165001 on 2/17/20.
+//  Copyright © 2020 POLYTECH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

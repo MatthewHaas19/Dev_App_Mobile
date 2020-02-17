fd92e077f24512d55cb1237646eda87195aba12f
MatthewHaas19 <44777911+MatthewHaas19@users.noreply.github.com>
2020-02-17 13:09:30 +0100

    server

---
+++ server/routes/users.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+var mongojs = require("mongojs")
+
+
+var db = mongojs("mongodb+srv://devmobileIG4:devmobileIG4@devmobile-vr63q.mongodb.net/DevMobile?retryWrites=true&w=majority",["users"])
+
+router.get("/users", function(req,res,next){
+  db.users.find(function(err,users){
+    if(err){
+      res.send(err);
+    }
+    res.json(users);
+  })
+})
+
+router.get("/users/:name", function(req,res,next){
+  const name = req.params.name
+  db.users.find({
+    email: name
+  },function(err,users){
+    if(err){
+      res.send(err);
+    }
+    res.json(users);
+  })
+})
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var index = require("./routes/index")
+var users = require("./routes/users")
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors())
+
+app.listen(process.env.PORT || 9090);
+
+//VIEWS
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine","ejs");
+app.engine("html",require("ejs").renderFile);
+
+// bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+//routes
+app.use("/", index)
+app.use("/users",users);

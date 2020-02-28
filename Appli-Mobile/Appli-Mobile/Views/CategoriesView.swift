e4ce3479c3582c40da4bfa6b7e3c0577498c1513
MatthewHaas19 <44777911+MatthewHaas19@users.noreply.github.com>
2020-02-28 14:51:15 +0100

    Catégories

--- Appli-Mobile/Appli-Mobile/Views/CategoriesView.swift
+++ Appli-Mobile/Appli-Mobile/Views/CategoriesView.swift
@@ -9,20 +9,20 @@
 import SwiftUI
 
 struct CategoriesView: View {
-    
+
     @Binding var isLogged:Bool
-    
+
     var body: some View {
 
-        
+
         VStack(alignment: .leading) {
-            
-            
-            
+
+
+
             // Stories Circles
             ScrollView([.horizontal], showsIndicators: false){
                 HStack {
-                    
+
                     self.isLogged ? VStack {
                         ZStack {
                             Image("Border2")
@@ -31,60 +31,135 @@ struct CategoriesView: View {
                         Text("Add Post")
                             .font(Font.system(size: 13.5))
                     }.padding(.trailing, 12) : nil
-                    
-                    
+
+
                     VStack {
                         ZStack {
                             Image("Border2")
-                            Image("Web")
+                            Image("All")
 
                         }
                         Text("All")
                             .font(Font.system(size: 13.5))
                     }.padding(.trailing, 12)
-                    
-                    
+
+
+                    VStack {
+                        ZStack {
+                            Image("Border2")
+                            Image("Amis")
+
+                        }
+                        Text("Entre amis")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+
+
+                    VStack {
+                        ZStack {
+                            Image("Border2")
+                            Image("Couple")
+                        }
+                        Text("En couple")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+
+
+                    VStack {
+                        ZStack {
+                            Image("Border2")
+                            Image("Ecole")
+                        }
+                        Text("En cours")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+
+                    VStack {
+                        ZStack {
+                            Image("Border2")
+                            Image("Famille")
+                        }
+                        Text("En famille")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+
+
                     VStack {
                         ZStack {
                             Image("Border2")
                             Image("Web")
+                        }
+                        Text("Sur internet")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
 
+
+                    VStack {
+                        ZStack {
+                            Image("Border2")
+                            Image("Rue")
                         }
                         Text("Dans la rue")
                             .font(Font.system(size: 13.5))
                     }.padding(.trailing, 12)
-                    
-                    
+
                     VStack {
                         ZStack {
                             Image("Border2")
-                            Image("AvatarBig1")
+                            Image("Soiree")
                         }
                         Text("En soirée")
                             .font(Font.system(size: 13.5))
                     }.padding(.trailing, 12)
-                   
-                    
+
                     VStack {
                         ZStack {
                             Image("Border2")
-                            Image("AvatarBig1")
+                            Image("Au sport")
                         }
-                        Text("Categorie3")
+                        Text("Sport")
                             .font(Font.system(size: 13.5))
                     }.padding(.trailing, 12)
-                    
+
                     VStack {
                         ZStack {
                             Image("Border2")
-                            Image("AvatarBig1")
+                            Image("Transport")
                         }
-                        Text("Categorie4")
+                        Text("Transport")
                             .font(Font.system(size: 13.5))
                     }.padding(.trailing, 12)
-                    
-                    
-                
+
+                    VStack {
+                        ZStack {
+                            Image("Border2")
+                            Image("TV")
+                        }
+                        Text("A la Télé")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+
+                    VStack {
+                        ZStack {
+                            Image("Border2")
+                            Image("Voisin")
+                        }
+                        Text("Voisins")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+
+
+                    VStack {
+                        ZStack {
+                            Image("Border2")
+                            Image("Autre")
+                        }
+                        Text("Autres")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+
+
+
                 }.padding(.leading, 16)
             }.padding(.leading,1)
              .padding(.trailing, 1)
@@ -92,7 +167,6 @@ struct CategoriesView: View {
         }.padding(.top)
         .padding(.bottom)
         .background(Color(red:0.95,green:0.95,blue:0.95))
-        
+
     }
 }
-

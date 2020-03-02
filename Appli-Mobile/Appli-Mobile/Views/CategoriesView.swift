97612a93a8c9718d277eb18923d0fc9374433bf9
Clemclem74 <44778853+Clemclem74@users.noreply.github.com>
2020-03-02 13:40:05 +0100

    Catégories finies

--- Appli-Mobile/Appli-Mobile/Views/CategoriesView.swift
+++ Appli-Mobile/Appli-Mobile/Views/CategoriesView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 
 struct CategoriesView: View {
     
-  
+    
     var body: some View {
-
+        
         
         VStack(alignment: .leading) {
             
@@ -24,65 +24,166 @@ struct CategoriesView: View {
                     
                     
                     
-                    VStack {
-                        ZStack {
-                            Image("Border2")
-                            Image("Web")
-
-                        }
-                        Text("All")
-                            .font(Font.system(size: 13.5))
-                    }.padding(.trailing, 12)
+                    HStack {
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("All")
+                                
+                            }
+                            Text("All")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Amis")
+                                
+                            }
+                            Text("Entre amis")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Couple")
+                            }
+                            Text("En couple")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Ecole")
+                            }
+                            Text("A l'école")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Famille")
+                            }
+                            Text("En famille")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        
+                    }
+                    
+                    HStack {
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Rue")
+                            }
+                            Text("Dans la rue")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Soiree")
+                            }
+                            Text("En soirée")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, CGFloat(12))
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Sport")
+                            }
+                            Text("Au sport")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Transport")
+                            }
+                            Text("Transports")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Travail")
+                            }
+                            Text("Au travail")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        
+                    }
+                    
+                    HStack{
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("TV")
+                            }
+                            Text("A la télé")
+                                .font(Font.system(size: CGFloat(13.5)))
+                        }.padding(.trailing, CGFloat(12))
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Voisin")
+                                
+                            }
+                            Text("Entre voisins")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Web")
+                                
+                            }
+                            Text("Web")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        VStack {
+                            ZStack {
+                                Image("Border2")
+                                Image("Autre")
+                                
+                            }
+                            Text("Autres")
+                                .font(Font.system(size: 13.5))
+                        }.padding(.trailing, 12)
+                        
+                        
+                    }
                     
                     
-                    VStack {
-                        ZStack {
-                            Image("Border2")
-                            Image("Web")
-
-                        }
-                        Text("Dans la rue")
-                            .font(Font.system(size: 13.5))
-                    }.padding(.trailing, 12)
                     
                     
-                    VStack {
-                        ZStack {
-                            Image("Border2")
-                            Image("AvatarBig1")
-                        }
-                        Text("En soirée")
-                            .font(Font.system(size: 13.5))
-                    }.padding(.trailing, 12)
-                   
-                    
-                    VStack {
-                        ZStack {
-                            Image("Border2")
-                            Image("AvatarBig1")
-                        }
-                        Text("Categorie3")
-                            .font(Font.system(size: 13.5))
-                    }.padding(.trailing, 12)
-                    
-                    VStack {
-                        ZStack {
-                            Image("Border2")
-                            Image("AvatarBig1")
-                        }
-                        Text("Categorie4")
-                            .font(Font.system(size: 13.5))
-                    }.padding(.trailing, 12)
                     
                     
-                
                 }.padding(.leading, 16)
             }.padding(.leading,1)
-             .padding(.trailing, 1)
-
+                .padding(.trailing, 1)
+            
         }.padding(.top)
-        .padding(.bottom)
-        .background(Color(red:0.95,green:0.95,blue:0.95))
+            .padding(.bottom)
+            .background(Color(red:0.95,green:0.95,blue:0.95))
         
     }
 }

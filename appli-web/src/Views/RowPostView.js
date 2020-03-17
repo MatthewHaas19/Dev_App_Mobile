719799973f7b1fe4292599aa982da2136d285663
Clemclem74 <44778853+Clemclem74@users.noreply.github.com>
2020-03-17 18:42:39 +0100

    Update RowPostView.js

--- appli-web/src/Views/RowPostView.js
+++ appli-web/src/Views/RowPostView.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import { purple } from '@material-ui/core/colors';
 import Card from '@material-ui/core/Card';
+import Container from '@material-ui/core/Container';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -10,7 +11,10 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Icon from '@material-ui/core/Icon';
-
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ExploreTwoToneIcon from '@material-ui/icons/ExploreTwoTone';
 
 
 const useStyles = makeStyles({
@@ -19,11 +23,38 @@ const useStyles = makeStyles({
     height:200,
     color:"white"
   },
+  content: {
+    margin:20,
+  },
   username: {
     fontSize: 14,
+    fontWeight: "bold",
   },
   texte: {
-    marginTop:20,
+    marginTop:18,
+  },
+  titre: {
+    fontSize:22,
+  },
+  note: {
+    fontSize:20,
+    fontWeight:"bold",
+  },
+  chevron: {
+    fontSize:40,
+    alignItems:"center",
+    color:"white"
+  },
+  notefleches:{
+    margin:0,
+  },
+  localisation: {
+    marginRight:20,
+    fontWeight:"bold",
+  },
+  logosTop: {
+    fontSize:20,
+    marginRight:10,
   },
 });
 
@@ -39,23 +70,30 @@ const RowPostView = (props) => {
       <Card >
       <CardActionArea>
   <CardContent className={classes.root} style={{ background: `rgb(${col})` }}>
-  <Grid container>
-  <Grid item xs={8}>
-    <Typography className={classes.username}  gutterBottom>
+  <Container className={classes.content}>
+  <Grid container alignItems="center">
+  <Grid item xs={1} align="right">
+  <AccountCircleIcon className={classes.logosTop}/>
+  </Grid>
+  <Grid item xs={7} >
+    <div className={classes.username} >
       {props.post.user}
-    </Typography>
+    </div>
     </Grid>
-    <Grid item xs={4}>
-    <Typography className={classes.localisation}  align="right" gutterBottom>
+    <Grid item xs={3} align="right">
+    <Typography className={classes.localisation}  gutterBottom>
      123 km
     </Typography>
+    </Grid>
+    <Grid item xs={1} align="left">
+    <ExploreTwoToneIcon className={classes.logosTop}/>
     </Grid>
   </Grid>
 
   <Grid container>
 
-    <Grid item xs={8}>
-      <Typography variant="h5" component="h2">
+    <Grid item xs={10}>
+      <Typography className={classes.titre}>
         {props.post.titre}
       </Typography>
       <Typography className={classes.texte} >
@@ -63,12 +101,25 @@ const RowPostView = (props) => {
       </Typography>
     </Grid>
 
-    <Grid item xs={4}>
-      <Icon className="keyboard_arrow_up"/>
+    <Grid item xs={2} className={classes.notefleches}>
+      <Grid container align="right">
+        <Grid item xs={12} align="center">
+          <Button><KeyboardArrowUpIcon className={classes.chevron} /></Button>
+        </Grid>
+        <Grid item xs={12}>
+          <div className={classes.note} align="center" >{props.post.note}</div>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button><KeyboardArrowDownIcon className={classes.chevron} /></Button>
+        </Grid>
+      </Grid>
+
+
+
     </Grid>
 
   </Grid>
-
+  </Container>
   </CardContent>
   </CardActionArea>
 </Card>
